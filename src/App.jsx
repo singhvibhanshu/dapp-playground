209a9e4759d45016c6e6eb7b56fe5ae281fdf113
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-08-31 21:10:30 +0530

    endpoint added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {
     WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
+import { airdrop } from './airdrop';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
@@ -18,12 +19,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <ConnectionProvider endpoint={endpoint}>
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
             <WalletProvider wallets={[]} autoConnect>
                 <WalletModalProvider>
-                  <>
-                    hi there
-                  </>
+                  <div>
+                    hi there <b>hello</b>
+                  </div>
+                  <airdrop />
                 </WalletModalProvider>
             </WalletProvider>
       </ConnectionProvider>
---
+++ src/airdrop.jsx
@@ -0,0 +1,8 @@
+export function airdrop() {
+  return (
+    <div>
+        <input type="text" placeholder="Enter the amount" />
+        <button>Send Airdrop</button>
+    </div>
+  )
+}
