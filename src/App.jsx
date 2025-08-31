60a101e7726e22f69cc3124b062b6e73bd7d6e07
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-08-31 20:48:53 +0530

    imported external dependencies for the dapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
 import { useState } from 'react'
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
 import './App.css'
 
 function App() {
